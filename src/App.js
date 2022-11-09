b55d5f69df95a8a4eb7b6bac03d7d7d745c7cde1
Mark Allen <markallenarchviz@gmail.com>
2022-11-09 16:17:22 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/settings" component={ Settings } />
     </Switch>
   );
 }
